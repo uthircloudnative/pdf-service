5e3726cda108ffddfc31083f5d50c8973754d82f
uthircloudnative <uthircloudnative@gmail.com>
2023-03-25 00:00:20 -0500

    Initial commit

---
+++ src/main/java/com/learntech/pdfmanager/pdfservice/PdfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learntech.pdfmanager.pdfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learntech/pdfmanager/pdfservice/PdfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntech.pdfmanager.pdfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
